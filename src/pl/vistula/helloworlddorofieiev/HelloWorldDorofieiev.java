78cfb99200faa092a32c6c7f482307959ebfe143
SoloEz <tinuzkaioh@gmail.com>
2022-10-06 19:15:40 +0200

    commit 2

--- src/pl/vistula/helloworlddorofieiev/HelloWorldDorofieiev.java
+++ src/pl/vistula/helloworlddorofieiev/HelloWorldDorofieiev.java
@@ -3,6 +3,11 @@ package pl.vistula.helloworlddorofieiev;
 public class HelloWorldDorofieiev {
     public static void main(String[] args) {
         System.out.println("HelloVistula");
+        System.out.println("HelloVistula");
+        System.out.println("HelloVistula");
+        System.out.println("HelloVistula");  System.out.println("HelloVistula");
+
+
 
     }
 
